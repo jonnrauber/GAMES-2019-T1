127111b4a46ae286237cab1df8bafb212e27723c
jonnrauber <natanrauber97@gmail.com>
2019-04-17 10:16:06 -0300

    Main menu title and buttons: play, settings, credits and quit

---
+++ source/MenuState.hx
@@ -0,0 +1,73 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxState;
+import flixel.FlxSprite;
+import flixel.text.FlxText;
+import flixel.ui.FlxButton;
+
+class MenuState extends FlxState
+{
+	var _title:FlxText;
+    var _btnPlay:FlxButton;
+    var _btnSettings:FlxButton;
+    var _btnCredits:FlxButton;
+    var _btnQuit:FlxButton;
+
+	override public function create():Void
+	{
+		_title = new FlxText("Nutter Boss");
+		_title.size = 20;
+		_title.screenCenter();
+		_title.y = 100;
+
+		_btnPlay = new FlxButton(0, 0, "START GAME", goPlay);
+		_btnPlay.screenCenter();
+
+		_btnSettings = new FlxButton(0, 0, "SETTINGS", goSettings);
+		_btnSettings.screenCenter();
+		_btnSettings.y = _btnPlay.y + _btnPlay.height + 10;
+
+		_btnCredits = new FlxButton(0, 0, "CREDITS", goCredits);
+		_btnCredits.screenCenter();
+		_btnCredits.y = _btnSettings.y + _btnSettings.height + 10;
+
+		_btnQuit = new FlxButton(0, 0, "QUIT", quit);
+		_btnQuit.screenCenter();
+		_btnQuit.y = _btnCredits.y + _btnCredits.height + 20;
+
+		add(_btnQuit);
+		add(_btnCredits);
+		add(_btnSettings);
+		add(_btnPlay);
+		add(_title);
+
+		super.create();
+	}
+
+	override public function update(elapsed:Float):Void
+	{
+		super.update(elapsed);
+	}
+
+	function goPlay()
+	{
+		FlxG.switchState(new PlayState());
+	}
+
+	function goSettings()
+	{
+		//TODO 
+	}
+
+	function goCredits()
+	{
+		//TODO 
+	}
+
+	function quit()
+	{
+		Sys.exit(0);
+	}
+
+}
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,11 +1,19 @@
 package;
 
 import flixel.FlxState;
+import flixel.FlxSprite;
 
 class PlayState extends FlxState
 {
+	var _player:FlxSprite;
+
 	override public function create():Void
 	{
+		_player = new FlxSprite();
+		_player.makeGraphic(20, 20, 0xffff0000);
+		_player.screenCenter();
+
+		add(_player);
 		super.create();
 	}
 
