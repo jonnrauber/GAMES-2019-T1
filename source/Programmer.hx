5081e0e654ffef2f310f2110e46328cdf5c6bc48
jonnrauber <natanrauber97@gmail.com>
2019-04-23 12:08:50 -0300

    add programmer tilesheet and animation

--- source/Programmer.hx
+++ source/Programmer.hx
@@ -2,6 +2,7 @@ package;
 
 import flixel.util.FlxPath;
 import flixel.FlxG;
+import flixel.FlxObject;
 import flixel.math.FlxPoint;
 
 class Programmer extends Entity
@@ -13,10 +14,36 @@ class Programmer extends Entity
         super();
         _timeToCoffee = 0;
         _workLocation = new FlxPoint(0, 0);
+        this.loadGraphic(AssetPaths.programmer__png, true, 32, 32);
+		this.setFacingFlip(FlxObject.LEFT, true, false);
+		this.setFacingFlip(FlxObject.RIGHT, false, false);
+		this.facing = FlxObject.RIGHT;
+        this.animation.add("up", [4,5,6,7], 15);
+        this.animation.add("down", [0,1,2,3], 15);
+        this.animation.add("left", [8,9,10,11], 15);
+        this.animation.add("right", [12,13,14,15], 15);
     }
 
     override public function update(elapsed:Float):Void {
         super.update(elapsed);
+
+        if (FlxG.keys.pressed.LEFT) {
+            this.x -= 1;
+            this.animation.play("left");
+        }
+        if (FlxG.keys.pressed.RIGHT) {
+            this.x += 1;
+            this.animation.play("right");
+        }
+        if (FlxG.keys.pressed.UP) {
+            this.y -= 1;
+            this.animation.play("up");
+        }
+        if (FlxG.keys.pressed.DOWN) {
+            this.y += 1;
+            this.animation.play("down");
+        }
+
 
         _timeToCoffee += 1;
     }
