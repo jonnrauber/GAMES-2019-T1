86aba442b0da71c30f8ffe9e539ecad40a84a4c5
jonnrauber <natanrauber97@gmail.com>
2019-05-07 09:58:35 -0300

    changed programmer spritesheet

--- source/Programmer.hx
+++ source/Programmer.hx
@@ -31,16 +31,16 @@ class Programmer extends Entity
 
         _workLocation = new FlxPoint(0, 0);
 
-        this.loadGraphic(AssetPaths.programmer__png, true, 32, 32);
+        this.loadGraphic(AssetPaths.programmer__png, true, 32, 48);
 		this.setFacingFlip(FlxObject.LEFT, true, false);
 		this.setFacingFlip(FlxObject.RIGHT, false, false);
 		this.facing = FlxObject.RIGHT;
 
-        this.animation.add("down", [0,1,2,3], 15);
-        this.animation.add("up", [4,5,6,7], 15);
-        this.animation.add("left", [8,9,10,11], 15);
-        this.animation.add("right", [12,13,14,15], 15);
-        this.animation.add("idle", [4], 15);
+        this.animation.add("down", [0,1,2,3], 20);
+        this.animation.add("left", [4,5,6,7], 20);
+        this.animation.add("right", [8,9,10,11], 20);
+        this.animation.add("up", [12,13,14,15], 20);
+        this.animation.add("idle", [12], 20);
         
         this.animation.play("idle");
     }
