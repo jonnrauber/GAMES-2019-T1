3b2d5696a7e4ae04c98722935047b6daa6b8d33e
jonnrauber <natanrauber97@gmail.com>
2019-05-07 13:35:33 -0300

    Added custom buttons to GameOverSubState

--- source/GameOverSubState.hx
+++ source/GameOverSubState.hx
@@ -17,20 +17,28 @@ class GameOverSubState extends FlxSubState {
     override public function create():Void {
         super.create();
 
-        _txtGameOver = new FlxText(0, 0, 0, "GAME OVER!", 20);
+        _txtGameOver = new FlxText(0, 0, 0, "Game Over!");
+        _txtGameOver.antialiasing = true;
+        _txtGameOver.setFormat(AssetPaths.Caveat_Bold__ttf, 36);
         _txtGameOver.screenCenter(FlxAxes.X);
         _txtGameOver.y = 100;
 
-        _txtTimeScore = new FlxText(0, 0, 0, "Time Score: ", 18);
+        _txtTimeScore = new FlxText(0, 0, 0, "Time Score: ");
+        _txtTimeScore.antialiasing = true;
+        _txtTimeScore.setFormat(AssetPaths.Quicksand_Regular__ttf, 20, 0xffffffff);
         _txtTimeScore.text += DateTools.format(Date.fromTime(_score), "%M:%S");
         _txtTimeScore.screenCenter(FlxAxes.X);
         _txtTimeScore.y = _txtGameOver.y + 50;
 
         _btnTryAgain = new FlxButton(0, 0, "Try Again", onTryAgain);
+        _btnTryAgain.label.setFormat(AssetPaths.Quicksand_Regular__ttf, 14, 0xff000000);
+		_btnTryAgain.loadGraphic(AssetPaths.generic_button__png, true, 176, 30);
         _btnTryAgain.screenCenter(FlxAxes.X);
         _btnTryAgain.y = _txtTimeScore.y + 50;
 
         _btnBackToMenu = new FlxButton(0, 0, "Leave to Menu", onBack);
+        _btnBackToMenu.label.setFormat(AssetPaths.Quicksand_Regular__ttf, 14, 0xff000000);
+		_btnBackToMenu.loadGraphic(AssetPaths.generic_button__png, true, 176, 30);
         _btnBackToMenu.screenCenter(FlxAxes.X);
         _btnBackToMenu.y = _btnTryAgain.y + 50;
 
